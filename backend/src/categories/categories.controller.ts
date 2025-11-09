0cec159fd531efb4a5978edf2ebe725e41f92f8f
Yevhen Tosych <zhenitosich21@gmail.com>
2025-11-09 02:33:13 +0100

    CRUD READY

--- backend/src/categories/categories.controller.ts
+++ backend/src/categories/categories.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import {CategoriesService} from './categories.service';
+import { Body, Controller, Get, Param, Post, Patch, Delete, } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
-@Controller('categories')
+@Controller({ path: 'categories' })
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
@@ -10,12 +11,23 @@ export class CategoriesController {
   findAll() {
     return this.categoriesService.findAll();
   }
+
   @Post()
   create(@Body() dto: CreateCategoryDto) {
     return this.categoriesService.create(dto);
   }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.categoriesService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateCategoryDto) {
+    return this.categoriesService.update(id, dto);
+  }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.categoriesService.remove(id);
   }
 }
--- backend/src/categories/categories.service.ts
+++ backend/src/categories/categories.service.ts
@@ -1,21 +1,11 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Category } from './category.entity'
+import { Category } from './category.entity';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
 export class CategoriesService {
-  async findOne(id: string) {
-    {
-      const category = await this.categoryRepository.findOneBy({ id: +id });
-
-      if (!category) {
-        throw new NotFoundException('Category not found');
-      }
-
-      return category;
-    }
-  }
   constructor(
     @InjectRepository(Category)
     private readonly categoryRepository: Repository<Category>,
@@ -24,9 +14,41 @@ export class CategoriesService {
   async findAll(): Promise<Category[]> {
     return this.categoryRepository.find();
   }
+
   async create(category: Partial<Category>): Promise<Category> {
     const newCategory = this.categoryRepository.create(category);
     return this.categoryRepository.save(newCategory);
   }
-}
 
+  async findOne(id: string) {
+    const category = await this.categoryRepository.findOneBy({ id: +id });
+
+    if (!category) {
+      throw new NotFoundException('Category not found');
+    }
+
+    return category;
+  }
+
+  async update(id: string, dto: UpdateCategoryDto) {
+    const category = await this.categoryRepository.findOneBy({ id: +id });
+
+    if (!category) {
+      throw new NotFoundException('Category not found');
+    }
+
+    category.name = dto.name;
+
+    return await this.categoryRepository.save(category);
+  }
+  async remove(id: string) {
+    const category = await this.categoryRepository.findOneBy({ id: +id });
+
+    if (!category) {
+      throw new NotFoundException('Category not found');
+    }
+    await this.categoryRepository.delete({ id: +id });
+
+    return true;
+  }
+}
