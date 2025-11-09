f03d12519f2e81355ac6f4faee509cffdd500739
Yevhen Tosychё <zhenitosich21@gmail.com>
2025-11-09 17:27:45 +0100

    validacija hotova vrode

--- backend/src/categories/DTO/create-category.dto.ts
+++ backend/src/categories/DTO/create-category.dto.ts
@@ -1,6 +1,8 @@
-import { IsString } from 'class-validator';
+import { IsString, MinLength, IsNotEmpty } from 'class-validator';
 
 export class CreateCategoryDto {
   @IsString()
+  @IsNotEmpty()
+  @MinLength(1 )
   name: string;
 }
