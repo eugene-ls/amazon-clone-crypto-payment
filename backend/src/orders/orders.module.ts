a1bbda87bad80050fedffd967a7866e3fbde6c8f
Yevhen Tosychё <zhenitosich21@gmail.com>
2025-11-10 14:24:54 +0100

    fix orders

--- backend/src/orders/orders.module.ts
+++ backend/src/orders/orders.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { OrderEntity } from './entities/order.entity/order.entity';
+import { OrderItem } from './entities/order-item.entity/order-item.entity';
+import { User } from '../users/entities/user.entity';
+import { Product } from '../products/entities/product.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([OrderEntity, OrderItem, User, Product]),
+  ],
+  controllers: [OrdersController],
   providers: [OrdersService],
-  controllers: [OrdersController]
+  exports: [OrdersService],
 })
 export class OrdersModule {}
