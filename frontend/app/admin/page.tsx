86d9e34ad819d9d81899cc8ede0a908b1964fd1e
Yevhen Tosychё <zhenitosich21@gmail.com>
2025-11-18 22:46:20 +0100

    lalalala

--- frontend/app/admin/page.tsx
+++ frontend/app/admin/page.tsx
@@ -35,37 +35,4 @@ export default function AdminPage() {
       {/* Тут твоя админка */}
     </div>
   );
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Admin Dashboard</h1>
-
-      <p>Welcome, {user.firstName}</p>
-
-      {/* Тут твоя админка */}
-    </div>
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Admin Dashboard</h1>
-
-      <p>Welcome, {user.firstName}</p>
-
-      {/* Тут твоя админка */}
-    </div>
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Admin Dashboard</h1>
-
-      <p>Welcome, {user.firstName}</p>
-
-      {/* Тут твоя админка */}
-      }
-    </div>
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Admin Dashboard</h1>
-
-      <p>Welcome, {user.firstName}</p>
-
-      {/* Тут твоя админка */}
-    </div>
 }
